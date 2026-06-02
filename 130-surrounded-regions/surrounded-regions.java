ef1886bf5e862c0a70444336a320e329d702ee42
Anshh Pasriccha <142911944+anshhpasriccha46@users.noreply.github.com>
2026-06-02 16:32:21 +0530

    Time: 228 ms (5.24%) | Memory: 58.1 MB (5.08%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,83 +1,84 @@
 import java.util.*;
-
-class Coord {
-    int x;
-    int y;
-
-    Coord(int x, int y) {
-        this.x = x;
-        this.y = y;
+class Coord{
+    public int x;
+    public int y;
+    Coord(int a , int b){
+        x = a;
+        y = b;
     }
 }
-
 class Solution {
-
-    int[] dx = {-1, 1, 0, 0};
-    int[] dy = {0, 0, -1, 1};
-
+     int dx[] = {-1 , 1 , 0 , 0};
+     int dy[] = {0 , 0 , -1 , 1};
+     Queue<Coord> Q = new LinkedList<>();
     public void solve(char[][] grid) {
-
-        int n = grid.length;
-        int m = grid[0].length;
-
-        boolean[][] visited = new boolean[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-
-                if (grid[i][j] == 'O' && !visited[i][j]) {
-
-                    ArrayList<Coord> component = new ArrayList<>();
-
-                    boolean touchesBorder =
-                            dfs(i, j, visited, grid, component);
-
-                    if (!touchesBorder) {
-
-                        for (Coord cell : component) {
-                            grid[cell.x][cell.y] = 'X';
-                        }
-
-                    }
+        // Code here
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        boolean done = false;
+        for(int i=0 ; i<grid.length ; i++){
+            for(int j=0 ; j<grid[0].length ; j++){
+                if(!visited[i][j] && grid[i][j] == 'O'){
+                    visited = new boolean[grid.length][grid[0].length];
+                     if(!dfs(new Coord(i , j) , visited , grid)){
+                      
+                         Q.add(new Coord(i , j));
+                     }
+                     
                 }
+                
             }
         }
+        visited = new boolean[grid.length][grid[0].length];
+        while(!Q.isEmpty()){
+            visited = new boolean[grid.length][grid[0].length];
+            changedfs(Q.poll() , visited, grid);
+        }
+        
+        
+        
     }
-
-    public boolean dfs(int r,
-                       int c,
-                       boolean[][] visited,
-                       char[][] grid,
-                       ArrayList<Coord> component) {
-
-        visited[r][c] = true;
-
-        component.add(new Coord(r, c));
-
-        boolean border = false;
-
-        if (r == 0 || r == grid.length - 1 ||
-            c == 0 || c == grid[0].length - 1) {
-
-            border = true;
+    
+    public boolean dfs(Coord zero , boolean[][] visited, char[][] grid ){
+        visited[zero.x][zero.y] = true;
+        if(zero.x == grid.length-1 || zero.x==0 || zero.y == grid[0].length-1 || zero.y==0){
+             visited[zero.x][zero.y] = false;
+            return true;
         }
-
-        for (int k = 0; k < 4; k++) {
-
-            int nr = r + dx[k];
-            int nc = c + dy[k];
-
-            if (nr >= 0 && nr < grid.length &&
-                nc >= 0 && nc < grid[0].length &&
-                grid[nr][nc] == 'O' &&
-                !visited[nr][nc]) {
-
-                if (dfs(nr, nc, visited, grid, component)) {
-                    border = true;
-                }
+        for(int i=0 ; i<4 ; i++){
+            int xc = zero.x + dx[i];
+            int yc = zero.y + dy[i];
+           
+           if(xc>=0 && xc<grid.length && yc>=0 && yc<grid[0].length){
+           
+            if(grid[xc][yc] == 'O' && !visited[xc][yc]){
+                
+                if( dfs(new Coord(xc , yc) , visited ,  grid) ) return true;
+                
+              
+            }
+           }
+        }
+        
+        
+        
+        return false;
+       
+        
+    }
+    public void changedfs(Coord zero, boolean[][] visited , char[][] grid){
+        visited[zero.x][zero.y] = true;
+        grid[zero.x][zero.y] = 'X';
+        for(int i=0 ; i<4 ; i++){
+             int xc = zero.x + dx[i];
+            int yc = zero.y + dy[i];
+            if(xc>=0 && xc<grid.length && yc>=0 && yc<grid[0].length)
+           
+            if(grid[xc][yc] == 'O' && !visited[xc][yc]){
+                
+                changedfs(new Coord(xc , yc) , visited , grid) ;
+                
+              
             }
         }
-
-        return border;
     }
 }
