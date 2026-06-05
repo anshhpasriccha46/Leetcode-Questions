f06620c3b1bc9936993c304e0777b8028ff7b252
Anshh Pasriccha <142911944+anshhpasriccha46@users.noreply.github.com>
2026-06-05 16:04:19 +0530

    Time: 32 ms (12.49%) | Memory: 64 MB (18.84%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        ArrayList<ArrayList<Integer>> g = new ArrayList<>();
+        for(int i =0 ; i<graph.length ; i++){
+            g.add(new ArrayList<>());
+        }
+        for(int to = 0 ; to<graph.length  ; to++){
+            for(int from: graph[to]){
+                g.get(from).add(to);
+            }
+        }
+        int[] indeg = new int[graph.length];
+      for(ArrayList<Integer> entry : g){
+        for(int i: entry){
+            indeg[i]++;
+        }
+      }
+      Queue<Integer> q = new LinkedList<>();
+     ArrayList<Integer> ans  = new ArrayList<>();
+      for(int i=0 ; i<g.size() ; i++){
+        if(indeg[i]==0){
+            q.add(i);
+            ans.add(i);
+            indeg[i]= -1;
+            
+        }
+      }
+      while(!q.isEmpty()){
+        int ele  = q.poll();
+        for(int i: g.get(ele)){
+            indeg[i]--;
+            if(indeg[i]==0){
+                q.add(i);
+                ans.add(i);
+                indeg[i] = -1;
+            }
+        }
+      }
+      Collections.sort(ans , (a , b)->{
+        return a-b;
+      });
+      return ans;
+     
+
+    }
+}
