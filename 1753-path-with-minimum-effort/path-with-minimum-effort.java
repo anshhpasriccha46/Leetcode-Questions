d1205ba4ef8482e380d6a59a0cb3edb15540bc63
Anshh Pasriccha <142911944+anshhpasriccha46@users.noreply.github.com>
2026-06-10 11:09:32 +0530

    Time: 312 ms (5.12%) | Memory: 48.6 MB (6.78%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class coord{
+    int x;
+    int y;
+    int diff;
+    coord(int a, int b , int c){
+        x = a;
+        y = b;
+        diff = c;
+    }
+}
+
+class Solution {
+    int dx[] = {-1 ,1,  0, 0};
+int dy[] = {0 , 0 , -1 , 1};
+    public int minimumEffortPath(int[][] heights) {
+        if(heights.length == 1 && heights[0].length == 1) return 0;
+        Queue<coord> q= new LinkedList<>();
+        q.add(new coord(0 , 0 , -1));
+        return kar(heights , q );
+    }
+    public int kar(int[][] h ,  Queue<coord> q){
+        int[][] best = new int[h.length][h[0].length];
+        for(int i=0; i<best.length  ; i++){
+            Arrays.fill(best[i] , Integer.MAX_VALUE);
+        }
+        while(!q.isEmpty()){
+            coord temp = q.poll();
+            for(int i=0 ; i<4 ; i++){
+                int xc = temp.x + dx[i];
+                int yc = temp.y + dy[i];
+                if(xc>=0 && yc>=0 && xc<h.length && yc<h[0].length){
+                     int a = Math.max(temp.diff,
+                 Math.abs(h[xc][yc] - h[temp.x][temp.y]));
+                if(best[xc][yc] > a ){
+                    best[xc][yc] = a;
+                    q.add(new coord(xc , yc , a));
+                 }
+                }
+               
+            }
+        }
+        return best[h.length - 1][h[0].length - 1];
+    }
+}
