70280033edc7ed0f834504bd200e73c3a68c3a55
Anshh Pasriccha <142911944+anshhpasriccha46@users.noreply.github.com>
2026-06-17 15:23:00 +0530

    Time: 13 ms (35.73%) | Memory: 75.7 MB (5.72%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,47 +1,41 @@
 class Solution {
-    public int makeConnected(int n, int[][] connections) {
-        int components = n;
-        int extra = 0;
+    int components = 0;
+    int edge = 0;
+    public int makeConnected(int n, int[][] edges) {
+          boolean visited[] = new boolean[n];
+          int count = 0;
+          edge = edges.length;
+          components= n;
+        ArrayList<ArrayList<Integer>> g = new ArrayList<>();
         
-
-        int[] rank = new int[n];
-        int[] parent = new int[n];
-        for(int i =0 ; i<parent.length ; i++){
-            parent[i] = i;
+        for(int i= 0 ; i<n ; i++){
+            g.add(new ArrayList<>() );
         }
-        for(int i=0 ; i<connections.length  ; i++){
-            int n1 = connections[i][0];
-            int n2 = connections[i][1];
-            if(findParent(n1 , parent) == findParent(n2 , parent)){
-                extra++;
-            }
-            else{
-                components--;
-              
-                union(n1 , n2 ,  parent , rank);
+        for(int i=0 ; i<edges.length ; i++){
+           g.get(edges[i][0]).add(edges[i][1]);
+            g.get(edges[i][1]).add(edges[i][0]);
+        }
+        for(int i=0 ; i<n ; i++){
+            if(visited[i] == false){
+                visited[i] = true;
+                count++;
+                found(i , -1 , g , visited);
+                
             }
         }
-        if( extra >= components - 1 ) return components - 1;
+        if(edge >= components - 1) return components -1;
         return -1;
     }
-    public int findParent(int n1 ,  int parent[]){
-        if(n1 == parent[n1]) return n1;
-        parent[n1] = findParent( parent[n1] , parent);
-        return parent[n1];
-    }
-    public void union(int n1 , int n2 , int parent[] , int rank[]){
-        int pn1 = findParent(n1 , parent);
-        int pn2 = findParent(n2 , parent);
-        if(rank[pn1] > rank[pn2]){
-            parent[pn2] = pn1;
+    public void found(int n ,int p, ArrayList<ArrayList<Integer>> g , boolean[] v ){
+        
+        for(int i : g.get(n)){
+            if(!v[i]){
+                v[i] = true;
+                components--;
+                edge--;
+                found(i , n , g , v );
+               
+            }
         }
-        else if(rank[pn1] < rank[pn2]){
-            parent[pn1] = pn2;
-        }
-        else{
-            parent[pn2] = pn1;
-            rank[pn1]++;
-        }
-
     }
 }
