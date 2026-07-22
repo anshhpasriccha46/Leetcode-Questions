ab73214025f303c05d9a773ee8012c1f0b4731ff
Anshh Pasriccha <142911944+anshhpasriccha46@users.noreply.github.com>
2026-07-22 20:30:28 +0530

    Time: 28 ms (7.47%) | Memory: 51.6 MB (5.30%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,48 +1,44 @@
 import java.util.*;
-class node{
-    int n;
+class Pair{
+    int node ;
     int time;
-    node(int a , int b){
-        n = a;
-        time  = b;
-
+    Pair(int a , int b){
+        node = a;
+        time = b;
     }
 }
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        ArrayList<ArrayList<node>> g = new ArrayList<>();
-        boolean[] v = new boolean[n+1];
-        int best[] = new int[n+1];
-        Arrays.fill(best , Integer.MAX_VALUE);
-        
-        for(int i=0  ; i<n+1 ; i++){
+        ArrayList<ArrayList<Pair>> g = new ArrayList<>();
+        for(int i=0 ; i<=n ; i++){
             g.add(new ArrayList<>());
         }
-        for(int i[] : times){
-            g.get(i[0]).add(new node(i[1] , i[2] ) ) ;
-
+        for(int entry[] : times){
+            g.get(entry[0]).add(new Pair(entry[1] , entry[2]));
         }
+        int best[] = new int[n+1];
+        Arrays.fill(best , Integer.MAX_VALUE);
         best[k] = 0;
-        Queue<Integer> q = new LinkedList<>();
-        q.add( k );
-        int timeTaken = Integer.MIN_VALUE;
-
-        while( !q.isEmpty() ){
-            int temp = q.poll();
-           for(node neigh : g.get(temp)){
-            int a = best[temp] + neigh.time;
-            if(best[neigh.n] > a){
-                best[neigh.n] = a;
-                q.add( neigh.n );
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(k , 0));
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+            int node = curr.node;
+            int time = curr.time;
+            for(Pair nei: g.get(node)){
+                if(best[nei.node] > best[node] + nei.time){
+                    best[nei.node] = best[node] + nei.time;
+                    System.out.println(nei.node + " " + best[nei.node]);
+                    q.add(new Pair(nei.node ,best[nei.node]  ));
+                }
             }
-           }
-            
-        } 
-       for(int i=1 ; i<n+1 ; i++){
-        timeTaken = Math.max(timeTaken , best[i]);
-       }
-       if(timeTaken == Integer.MAX_VALUE) return -1;
-        return timeTaken;
-
+        }
+        int ans = 0;
+        best[0] = 0;
+        for(int i: best){
+            if(i==Integer.MAX_VALUE) return -1;
+            ans = Math.max(ans , i);
+        }
+        return ans;
     }
 }
