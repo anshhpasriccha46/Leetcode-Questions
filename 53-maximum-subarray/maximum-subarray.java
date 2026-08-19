32055ee48e3bed8a206a0c65f9411b64bc1e005f
Anshh Pasriccha <142911944+anshhpasriccha46@users.noreply.github.com>
2026-08-19 18:33:23 +0530

    Time: 1 ms (99.95%) | Memory: 77.2 MB (66.98%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,18 +1,23 @@
-import java.util.* ;
-import java.io.*; 
+class pair{
+    int x;
+    int y;
+    pair(int a , int b){
+        x = a;
+        y = b;
+    }
+}
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int currSum = 0;
+        for(int i : nums){
+            if(currSum<0 && i>=currSum) currSum = i;
+           else currSum+=i;
+            max = Math.max(max , currSum);
+        }
+        return max;
+        
+      
 
-public class Solution {
-	
-	public int maxSubArray(int[] arr) {
-		// write your code here
-		int curr = arr[0];
-		int max = arr[0];
-		for(int i=1 ; i<arr.length ; i++){
-			
-			curr = Math.max(  arr[i] , curr + arr[i]  );
-			max = Math.max(curr , max);
-		}
-		return max;
-	}
-
+    }
 }
