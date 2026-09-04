1c82319bd825daa6381f8804e86fdbf54ac6b392
Anshh Pasriccha <142911944+anshhpasriccha46@users.noreply.github.com>
2026-09-04 14:27:57 +0530

    Time: 746 ms (22.86%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,6 +1,15 @@
-# Write your MySQL query statement below
--- select sum(
---     if(a.tiv_2015 in (select tiv_2015 from insurance where pid!=a.pid and tiv_2015 = a.tiv_2015 and (lat!=a.lat or lon!=a.lon)) , a.tiv_2016 , null)
--- ) as tiv_2016 from insurance a ;
--- select a.* from insurance a where a.tiv_2015 in (select tiv_2015 from insurance b where b.pid!=a.pid and b.tiv_2015 = a.tiv_2015 and (b.lat , b.lon) not in(select lat, lon from insurance c where c.pid!=b.pid))
-select round(sum(b.tiv_2016) , 2) as tiv_2016 from insurance b where (b.lat , b.lon) not in(select lat, lon from insurance c where c.pid!=b.pid) and b.tiv_2015 in (select tiv_2015 from insurance c where c.pid!=b.pid);
+SELECT ROUND(SUM(i.tiv_2016), 2) AS tiv_2016
+FROM Insurance i
+WHERE i.pid IN (
+    SELECT DISTINCT a.pid
+    FROM Insurance a
+    JOIN Insurance b
+        ON a.pid <> b.pid
+        AND a.tiv_2015 = b.tiv_2015
+    WHERE (a.lat, a.lon) IN (
+        SELECT lat, lon
+        FROM Insurance
+        GROUP BY lat, lon
+        HAVING COUNT(*) = 1
+    )
+);
