88602e4a87cb35f0883fdeecc6f113e754df39fc
Anshh Pasriccha <142911944+anshhpasriccha46@users.noreply.github.com>
2026-09-04 12:12:56 +0530

    Time: 515 ms (62.08%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,5 +1,7 @@
-# Write your MySQL query statement below
-select a.product_id , a.new_price as price from products a where a.change_date <= '2019-08-16' and change_date = (select max(change_date) from products where product_id = a.product_id and change_date <= '2019-08-16' ) 
+-- # Write your MySQL query statement below
+-- select product_id , new_price as price from products p join (select a.product_id, a.new_price , max(a.change_date) from (select b.* from products b where change_date<='2019-08-16') a group by a.product_id) where change_date<='2019-08-16';
+-- -- union
+-- -- select from products where change_date<='2019-08-16';
+select a.product_id , a.new_price as price from products a join (select b.product_id as product_id ,  max(b.change_date) as max_date from products b where b.change_date<='2019-08-16' group by b.product_id) c on a.product_id = c.product_id and a.change_date = c.max_date
 union
-select product_id , 10 as price  from products where product_id not in( select product_id from products where change_date<='2019-08-16')
-order by product_id asc;
+select product_id , 10 as price from products where product_id not in (select p.product_id from products p where change_date<='2019-08-16');
